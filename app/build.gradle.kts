82bae47b11ae0be71131bcce0b9e1b665162ddc2
teddyayomi <arccodemix@gmail.com>
2026-02-04 13:13:03 +0700

    Fix build: Hardcode KSP plugin version to bypass catalog issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.hilt.android)
-    alias(libs.plugins.ksp)
+    id("com.google.devtools.ksp")
 }
 
 android {
